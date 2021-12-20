cb46f654f7d73c0a2ec7dc9e07803a55ff192f24
felipe <feload@gmail.com>
2021-12-20 14:41:29 -0600

    Remove required auth to create users

--- app/routes/user.py
+++ app/routes/user.py
@@ -11,7 +11,7 @@ router = APIRouter(
 
 
 @router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth.get_current_user)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     user.password = utils.hash(user.password)
     new_user = models.User(**user.dict())
